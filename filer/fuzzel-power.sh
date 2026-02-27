84cd4d60bdb42180f5fa7350294512a2f75c68c9
albert <albertsen.lindberg@gmail.com>
2026-02-27 19:47:39 +0100

    power menu

---
+++ filer/fuzzel-power.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Options
+options=" Poweroff\n Reboot\n Lock\n"
+
+# Selection
+selection=$(echo -e "$options" | fuzzel --dmenu --prompt "Power > " | awk '{print $2}')
+
+# Action
+case "$selection" in
+    Poweroff) systemctl poweroff ;;
+    Reboot) systemctl reboot ;;
+    Lock) swaylock ;; # Change to your locker, e.g., hyprlock
+esac
